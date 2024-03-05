63e801c2d354e976d8e9a261f5c83da6b67e0814
Jazz <jcronin@unca.edu>
2024-03-04 19:59:22 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,48 @@
+// App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+
+import HomeScreen from './HomeScreen';
+import ProfileScreen from './ProfileScreen';
+import SettingsScreen from './SettingsScreen';
+import RandomScreen from './RandomScreen';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (route.name === 'Profile') {
+              iconName = focused ? 'person' : 'person-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'settings' : 'settings-outline';
+            } else if (route.name === 'Random') {
+              iconName = focused ? 'apps' : 'apps-outline';
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Random" component={RandomScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
---
+++ RandomScreen.js
@@ -0,0 +1,13 @@
+// RandomScreen.js
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const RandomScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Random Screen</Text>
+    </View>
+  );
+}
+
+export default RandomScreen;
